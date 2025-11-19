2b6b77b26d28e0da16e41a9c027941f3558e8e89
Fikri Ajiwijaya <fikri.ajiwijaya@gmail.com>
2025-11-19 06:25:16 +0000

    Variables and sections

--- chapter-2.lean
+++ chapter-2.lean
@@ -386,3 +386,53 @@ namespace chapter2_experiment38
   -- #check f7
   #check f8
 end chapter2_experiment38
+
+namespace chapter2_example39
+  def compose (α β γ : Type) (g : β → γ) (f : α → β) (x : α) : γ :=
+    g (f x)
+
+  def doTwice (α : Type) (h : α → α) (x : α) : α :=
+    h (h x)
+
+  def doThrice (α : Type) (h : α → α) (x : α) : α :=
+    h (h (h x))
+end chapter2_example39
+
+namespace chapter2_example40
+  variable (α β γ : Type)
+
+  def compose (g : β → γ) (f : α → β) (x : α) : γ :=
+    g (f x)
+
+  def doTwice (h : α → α) (x : α) : α :=
+    h (h x)
+
+  def doThrice (h : α → α) (x : α) : α :=
+    h (h (h x))
+end chapter2_example40
+
+namespace chapter2_example41
+  variable (α β γ : Type)
+  variable (g : β → γ) (f : α → β) (h : α → α)
+  variable (x : α)
+
+  def compose := g (f x)
+  def doTwice := h (h x)
+  def doThrice := h (h (h x))
+
+  #print compose
+  #print doTwice
+  #print doThrice
+end chapter2_example41
+
+namespace chapter2_example42
+  section useful
+    variable (α β γ : Type)
+    variable (g : β → γ) (f : α → β) (h : α → α)
+    variable (x : α)
+
+    def compose := g (f x)
+    def doTwice := h (h x)
+    def doThrice := h (h (h x))
+  end useful
+end chapter2_example42
