599572ab167338b1e63171ac1bf80808341c8f45
Fikri Ajiwijaya <fikri.ajiwijaya@gmail.com>
2025-11-28 11:44:15 +0000

    Working with propositions as types

--- chapter-3.lean
+++ chapter-3.lean
@@ -43,3 +43,117 @@ namespace chapter3_example4
 
   axiom implies_intro : (p q : Prop) → (Proof p → Proof q) → Proof (Implies p q)
 end chapter3_example4
+
+namespace chapter3_example5
+  set_option linter.unusedVariables false
+
+  variable {p : Prop}
+  variable {q : Prop}
+
+  theorem t1 : p → q → p := fun hp : p => fun hq : q => hp
+end chapter3_example5
+
+namespace chapter3_example6
+  set_option linter.unusedVariables false
+
+  variable {p q : Prop}
+
+  theorem t1 : p → q → p := fun hp : p => fun hq : q => hp
+
+  #print t1
+end chapter3_example6
+
+namespace chapter3_example7
+  set_option linter.unusedVariables false
+
+  variable {p q : Prop}
+
+  theorem t1 : p → q → p :=
+    fun hp : p =>
+    fun hq : q =>
+    show p from hp
+end chapter3_example7
+
+namespace chapter3_example8
+  set_option linter.unusedVariables false
+
+  variable {p q : Prop}
+
+  theorem t1 (hp : p) (hq : q) : p := hp
+
+  #print t1
+end chapter3_example8
+
+namespace chapter3_example9
+  set_option linter.unusedVariables false
+
+  variable {p q : Prop}
+
+  theorem t1 (hp : p) (hq : q) : p := hp
+
+  axiom hp : p
+
+  theorem t2 : q → p := t1 hp
+end chapter3_example9
+
+namespace chapter3_example10
+  axiom unsound : False
+
+  theorem ex : 1 = 0 :=
+    False.elim unsound
+end chapter3_example10
+
+namespace chapter3_example11
+  set_option linter.unusedVariables false
+
+  theorem t1 {p q : Prop} (hp : p) (hq : q) : p := hp
+
+  #print t1
+end chapter3_example11
+
+namespace chapter3_example12
+  set_option linter.unusedVariables false
+
+  theorem t1 : ∀ {p q : Prop}, p → q → p :=
+    fun {p q : Prop} (hp : p) (hq : q) => hp
+end chapter3_example12
+
+namespace chapter3_example13
+  set_option linter.unusedVariables false
+
+  variable {p q : Prop}
+
+  theorem t1 : p → q → p := fun (hp : p) (hq : q) => hp
+end chapter3_example13
+
+namespace chapter3_example14
+  set_option linter.unusedVariables false
+
+  variable {p q : Prop}
+  variable (hp : p)
+
+  -- theorem t1 : q → p := fun (hq : q) => hp
+end chapter3_example14
+
+namespace chapter3_example15
+  set_option linter.unusedVariables false
+
+  theorem t1 (p q : Prop) (hp : p) (hq : q) : p := hp
+
+  variable (p q r s : Prop)
+
+  #check t1 p q
+  #check t1 r s
+  #check t1 (r → s) (s → r)
+
+  variable (h : r → s)
+  #check t1 (r → s) (s → r) h
+end chapter3_example15
+
+namespace chapter3_example16
+  variable (p q r s : Prop)
+
+  theorem t2 (h₁ : q → r) (h₂ : p → q) : p → r :=
+    fun h₃ : p =>
+    show r from h₁ (h₂ h₃)
+end chapter3_example16
